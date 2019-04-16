1404c658e5d548443f226c749abf7d4bd4ced7fc
相田太一 <aidataichi@aidataichinoMacBook-Pro.local>
2019-04-16 23:34:44 +0900

    model

--- model.py
+++ model.py
@@ -15,22 +15,27 @@ import torch.nn.functional as F
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"{device}:{type(device)}")
 
+
+################################################
+# Loading data files
+################################################
+
 SOS_token = 0
 EOS_token = 1
 
 class Lang:
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
         self.word2index = {}
-        self word2count = {}
+        self.word2count = {}
         self.index2word = {0:"SOS", 1:"EOS"}
         self.n_words = 2
 
-    def addSentence(self,sentence):
+    def addSentence(self, sentence):
         for word in sentence.split(" "):
             self.addWord(word)
 
-    def addWord(self,word):
+    def addWord(self, word):
         if word not in self.word2index:
             self.word2index[word] = self.n_words
             self.word2count[word] = 1
@@ -38,3 +43,166 @@ class Lang:
             self.n_words += 1
         else:
             self.word2count[word] += 1
+
+
+# Unicode -> ASCII.
+def unicode2Ascii(s):
+    return "".join(
+        c for c in unicodedata.normalize("NFD",s)
+        if unicodedata.category(c) != "Mn"
+    )
+
+# Normalization.
+def normalizeString(s):
+    s = unicode2Ascii(s.lower().strip())
+    s = re.sub(r"([.!?])",r" \1",s)
+    s = re.sub(r"[^a-zA-Z.!?]+",r" ",s)
+    return s
+
+
+# To read the data.
+# File -> lines, and split into pairs. 
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+    
+    # Read the file and split into lines.
+    lines = open("~/lab/aida/conf_nmt/data.txt" % (lang1, lang2), encoding="utf-8").read().strip().split("\n")
+
+    # Split every line into pairs and normalize.
+    pairs = [[normalizeString(s) for s in l.split("\t")] for l in lines]
+    
+    # Reverse pairs, make Lang instances.
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+
+# To train quickly, we prepare the data set.
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+def filterPair(p):
+    return len(p[0].split(" ")) < MAX_LENGTH and \
+        len(p[1].split(" ")) < MAX_LENGTH and \
+            p[1].startswith(eng_prefixes)
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+
+# Full process for preparing the data.(Eng -> Other)
+# If you translate Other -> Eng, you should add reverse. ("reverse=True")
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    pairs = filterPairs(pairs)
+    print("Trimmed to %s sentence pairs" % len(pairs))
+    print("Counting words...")
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+print(random.choice(pairs))
+
+
+################################################
+# Seq to seq model
+################################################
+
+# The Encoder
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+# The Decoder
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+# Attention
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
