8fe8ae46ca0ff4bb0c38b4e88e488d2117fbafac
TaichiAida <ai.the.taichi@gmail.com>
2019-04-23 03:20:03 +0900

    data_fix

--- data_fix.py
+++ data_fix.py
@@ -1,11 +1,13 @@
 # Fix datasets -> (lang1)\t(lang2)
 import re
+import MeCab
 
 # Define where is your datasets.
 # data_place = "/Users/aidataichi/Downloads/ASPEC/ASPEC-JE/train/train-1.txt"
 # data_place = "/Users/aidataichi/Downloads/ASPEC/ASPEC-JE/test/test.txt"
 # data_place = "/Users/aidataichi/Downloads/ASPEC/ASPEC-JE/dev/dev.txt"
 data_place = "/Users/aidataichi/Downloads/ASPEC/ASPEC-JE/devtest/devtest.txt"
+mecab = MeCab.Tagger("-Owakati")
 
 with open(data_place) as data:
     lines = data.readlines()
@@ -14,12 +16,14 @@ with open(data_place) as data:
         # (similarity score) ||| (Field of symbols - Document ID) ||| (Sentences ID) ||| (Japanese) ||| (English)
         """
         sentences = line.split(" ||| ")
+        sentences[3] = mecab.parse(sentences[3])
         bilingual = "\t".join(sentences[3:])
         print(bilingual,end="")
         """
         # else
         # (Field of symbols - Document ID) ||| (Sentences ID) ||| (Japanese) ||| (English)
         sentences = line.split(" ||| ")
+        sentences[2] = mecab.parse(sentences[2])
         bilingual = "\t".join(sentences[2:])
         print(bilingual,end="")
 
