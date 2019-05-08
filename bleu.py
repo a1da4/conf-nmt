872acfd4e09bb0f79714588f7906890c38385b21
TaichiAida <AI.the.taichi@gmail.com>
2019-05-08 14:39:39 +0900

    bleu

--- bleu.py
+++ bleu.py
@@ -3,3 +3,19 @@
 # n-gram: 1 <= n <= 4
 # called by "model.py" or "main.py"
 
+def ngram():
+# n-gram overlap
+# calculate n(1~4)-gram precision.
+
+
+def penalty():
+# brevity penalty
+# calculate min(1, exp(1 - reference-length/output-length))
+
+def bleu():
+# calculate (brevity penalty) * (n-gram overlap)
+
+if __name__ == "__main__":
+    bleuScore = bleu()
+    return bleuScore
+
