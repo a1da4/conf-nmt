4836a603ac6dbe359189d539cab293ef7d3775b8
TaichiAida <AI.the.taichi@gmail.com>
2019-05-10 12:31:10 +0900

    bleu score

--- bleu.py
+++ bleu.py
@@ -4,11 +4,20 @@
 # called by "model.py" or "main.py"
 import math
 
-def ngram():
-# n-gram overlap
-# calculate n(1~4)-gram precision.
+def splitAndNormalize(sentence):
+    # normalize
+    # split
 
 
+def ngram(sentence, n):
+    # make sentence into n-gram
+
+
+def ngram_precision():
+    # n-gram overlap
+    # calculate n(1~4)-gram precision.
+    
+    
 def brevity_penalty(target_sentence_split, output_sentence_split):
     # brevity penalty
     # calculate min(1, exp(1 - reference-length/output-length))
@@ -20,8 +29,9 @@ def brevity_penalty(target_sentence_split, output_sentence_split):
 
 
 def bleu(target_sentence, output_sentence):
-# calculate (brevity penalty) * (n-gram overlap)
-# use target and output sentence
+    # calculate (brevity penalty) * (n-gram overlap)
+    # use target and output sentence
+
 
 if __name__ == "__main__":
     bleuScore = bleu(target_sentence, output_sentence)
