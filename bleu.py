66b40c202429a3a21439593c246a99c4bc933776
TaichiAida <AI.the.taichi@gmail.com>
2019-05-10 12:13:36 +0900

    bleu score

--- bleu.py
+++ bleu.py
@@ -2,20 +2,28 @@
 # calculate bleu score of nmt results.
 # n-gram: 1 <= n <= 4
 # called by "model.py" or "main.py"
+import math
 
 def ngram():
 # n-gram overlap
 # calculate n(1~4)-gram precision.
 
 
-def penalty():
-# brevity penalty
-# calculate min(1, exp(1 - reference-length/output-length))
+def brevity_penalty(target_sentence_split, output_sentence_split):
+    # brevity penalty
+    # calculate min(1, exp(1 - reference-length/output-length))
+    # must separated(use .split(" "))
+    target_length = len(target_sentence_split)
+    output_length = len(output_sentence_split)
+    lengthRateEXP = math.exp(1 - target_length/output_length)
+    return min(1, lengthRateEXP)
 
-def bleu():
+
+def bleu(target_sentence, output_sentence):
 # calculate (brevity penalty) * (n-gram overlap)
+# use target and output sentence
 
 if __name__ == "__main__":
-    bleuScore = bleu()
+    bleuScore = bleu(target_sentence, output_sentence)
     return bleuScore
 
