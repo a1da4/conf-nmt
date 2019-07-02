3626253dfad5fd4cdd428640a66582afe2942773
TaichiAida <ai.the.taichi@gmail.com>
2019-07-03 03:04:38 +0900

    .txt to .csv

---
+++ text2csv.py
@@ -0,0 +1,41 @@
+# text data -> excel
+# S, T, H, P-(id)"\t"
+
+import re
+import csv
+
+text = "fairseq_result.txt"
+
+def getOutputs(text):
+    with open(text) as data:
+        lines = data.readlines()
+        sentences = []
+        outputs = []
+        flag = 0
+        for line in lines:
+            if flag:
+                lines = line.split("\t")
+                if line[0] == "H":
+                    outputs.append(lines[2])
+                outputs.append(lines[1])
+                if line[1] == "'":
+                    flag = 0
+
+            if line[0] == "S":
+                flag = 1
+                #print(outputs)
+                sentences.append(outputs)
+                outputs = []
+                outputs.append(line.split("\t")[1])
+    return sentences
+
+
+def write(sentences):
+    with open("result.csv", "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for sentence in sentences:
+            writer.writerow(sentence)
+
+sentences = getOutputs(text)
+write(sentences)
+
